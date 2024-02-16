12f2e0b348ea90aaa8c14449c3219444d7c20c6a
hdmor <hdmor.h@gmail.com>
2024-02-16 15:40:27 +0330

    Initialize project and add dependencies

---
+++ app/src/main/java/m/eight/texingram/MainActivity.kt
@@ -0,0 +1,24 @@
+package m.eight.texingram
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import m.eight.texingram.ui.theme.TexingramTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TexingramTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    //
+                }
+            }
+        }
+    }
+}
