c24e8793730fd7e4ec6ccb33704c8752881d50c6
hdmor <hdmor.h@gmail.com>
2024-02-17 01:18:27 +0330

    Setup navController in MainActivity.kt

--- app/src/main/java/m/eight/texingram/MainActivity.kt
+++ app/src/main/java/m/eight/texingram/MainActivity.kt
@@ -3,20 +3,32 @@ package m.eight.texingram
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import dagger.hilt.android.AndroidEntryPoint
+import m.eight.texingram.presentation.chat.ChatScreen
+import m.eight.texingram.presentation.username.UsernameScreen
 import m.eight.texingram.ui.theme.TexingramTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TexingramTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    //
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "username_screen") {
+                    composable(route = "username_screen") { UsernameScreen(onNavigate = navController::navigate) }
+                    composable(
+                        route = "chat_screen/{username}",
+                        arguments = listOf(navArgument("username") {
+                            type = NavType.StringType
+                            nullable = true
+                        })
+                    ) { ChatScreen(username = it.arguments?.getString("username")) }
                 }
             }
         }
