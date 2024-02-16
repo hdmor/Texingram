014958a6b3c7e67ee7cbbcd1f995adfeae251678
hdmor <hdmor.h@gmail.com>
2024-02-16 16:05:33 +0330

    Add a convertToTimeAgo function into Common.kt class to get data as timeAgo format and also define a mapper function to convert api(messageDto) model to ui(message) model

--- app/src/main/java/m/eight/texingram/data/remote/dto/MessageDto.kt
+++ app/src/main/java/m/eight/texingram/data/remote/dto/MessageDto.kt
@@ -1,6 +1,8 @@
 package m.eight.texingram.data.remote.dto
 
 import kotlinx.serialization.Serializable
+import m.eight.texingram.domain.model.Message
+import m.eight.texingram.util.convertToTimeAgo
 
 @Serializable
 data class MessageDto(
@@ -8,4 +10,7 @@ data class MessageDto(
     val username: String,
     val message: String,
     val timestamp: Long
-)
+) {
+    fun toMessage(): Message =
+        Message(username = username, message = message, timestamp = convertToTimeAgo(timestamp))
+}
